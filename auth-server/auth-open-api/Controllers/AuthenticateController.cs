458ad2b4ab21dc64c5d9b89bdec3eeeb88e2b03b
Valeriy Chutchenko <valeriy.chutchenko@gsapps.com>
2016-11-05 17:06:33 +0200

    Fix: check password

--- auth-server/auth-open-api/Controllers/AuthenticateController.cs
+++ auth-server/auth-open-api/Controllers/AuthenticateController.cs
@@ -33,9 +33,8 @@ namespace Achi.Server.Controllers
 			var user = await ApiCallSession.DB.GetDocument("user",cr.login);
 			if (user["error"] != null) return this.Unauthorized();
 
-			string passwordHash = user["password"].ToString();
-			string receivedPasswordHash = AuthSecurity.GetPasswordHash(cr.password);
-			if (!AuthSecurity.IsPasswordValid(passwordHash, receivedPasswordHash)) return Unauthorized();
+			string passwordHash = user["password"].ToString();			
+			if (!AuthSecurity.IsPasswordValid(passwordHash, cr.password)) return Unauthorized();
 
 			var doc = await CreateNewToken(cr.login);
 
